4fa6e5e35300f53c0f4ff66579586bd9b6d52816
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-09-04 03:53:34 -0300

    Update

--- components/borders.tsx
+++ components/borders.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import { Button } from "@nextui-org/button";
 
 import Country from "@/config/interfaceCountries";
+import { fetchCountries } from "@/services/countries";
 
 interface BordersProps {
   country: Country;
 }
 
 export default function Borders({ country }: BordersProps) {
+  const [countryList, setCountryList] = useState<Country[]>([]);
   const borders = country.borders || [];
+
+  useEffect(() => {
+    const fetchCountryList = async () => {
+      const data = await fetchCountries();
+
+      setCountryList(data);
+    };
+
+    fetchCountryList();
+  }, []);
+
+  const getCountryNameByCCA3 = (cca3: string) => {
+    const country = countryList.find((c) => c.cca3 === cca3);
+
+    return country ? country.name.common : cca3;
+  };
 
   return (
     <div className="flex flex-row gap-4 flex-wrap">
@@ -18,7 +37,7 @@ export default function Borders({ country }: BordersProps) {
             key={index}
             className="h-7 rounded-sm shadow-small bg-veryLightGray dark:bg-darkBlue place-self-start px-10 gap-1"
           >
-            {border}
+            {getCountryNameByCCA3(border)}
           </Button>
         ))
       ) : (
